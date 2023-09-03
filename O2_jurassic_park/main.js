6c8966d6d229cd6875be5fd29525dbbbd39bae4a
SimSarz <115283822+SimSarz@users.noreply.github.com>
2023-09-03 10:42:07 -0400

    ajout du test technique pour O2Web

---
+++ O2_jurassic_park/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slick-carousel').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+    });
+});
+
+
